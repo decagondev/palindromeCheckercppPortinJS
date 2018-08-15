77a895d29d63f52484c1e50ba39685d130c4154f
decagondev <decagondevelopment@gmail.com>
2018-08-15 04:02:38 +0100

    added a few tests

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ function isPalindrome(s)
         return (s[0] == s[s.length - 1] && isPalindrome(s.substr(1, s.length - 2)));
 }
 
-console.log(isPalindrome("ermom"));
+console.log(isPalindrome("ermom")); // --> false
+console.log(isPalindrome("mom")); // --> true
+console.log(isPalindrome("dad")); // --> true
+console.log(isPalindrome("sad")); // --> false
